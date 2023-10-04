4d9dfcda49c512bf02ef2c51458968d6f9c1d294
Dannel Lim Khye Hsen <dannel.limkhyehsen@azqore.com>
2023-10-04 17:55:47 +0800

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Dannel Lim on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
